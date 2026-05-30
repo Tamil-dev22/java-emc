da948deb3612c9cc2e4403d1762fe48887bb71bf
Tamilarasan <tamilarasanpr22@gmail.com>
2026-05-30 22:24:37 +0530

    problems solved

--- hello.java
+++ hello.java
@@ -25,7 +25,7 @@ class hello {
       input.nextLine();
       String department =  input.nextLine();
       System.out.println("My name is " + name);
-      System.out.println("My score is" + score/10 + "/10");
+      System.out.println("My score is " + score/10 + "/10");
       System.out.print("My department is " + department);
     }
 }
