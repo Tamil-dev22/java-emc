230c761a9a4955981646cbbd63b2dae8f652ba83
Tamilarasan <tamilarasanpr22@gmail.com>
2026-05-31 11:46:57 +0530

    String comparison done

--- compare.java
+++ compare.java
@@ -1,15 +1,28 @@
+
 import java.util.Scanner;
 
 class compare {
-    public static void main(String args[]){
-   Scanner scan = new Scanner(System.in);
-   int num1 =  scan.nextInt();
-   int num2 =  scan.nextInt();
-   if(num1 == num2){
-    System.out.print("Both are Equal");
-   }else{
-    System.out.print("Not Equal");
-   }
+
+    public static void main(String args[]) {
+        Scanner scan = new Scanner(System.in);
+//    int num1 =  scan.nextInt();
+//    int num2 =  scan.nextInt();
+//    if(num1 == num2){
+//     System.out.print("Both are Equal");
+//    }else{
+//     System.out.print("Not Equal");
+//    }
+
+//     }
+        String rcb = scan.nextLine();
+
+        if (rcb.equals("win")) {
+            System.out.println("Ee sala cup Namdhey");
+        } else {
+            System.out.println("No Cup");
+
+        }
 
     }
+
 }
